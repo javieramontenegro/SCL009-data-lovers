e738b6d0de924d05f7378f61f7e26a03751554ce
javi monte <doki.snsd@gmail.com>
2019-05-01 22:18:22 -0400

    avance css

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 
 /* Manejo del DOM */
 const pokeData=POKEMON.pokemon;
-const orderDos = document.querySelectorAll("button.ordered-by")
+const order = document.querySelectorAll("button.ordered-by")
 const container = document.getElementById("show-data")
-//console.log(orderDos);
+//console.log(order);
 
 // LISTENER PARA LLAMAR A LA FUNCION SORT FINALLL
  
-orderDos.forEach(element => {
+order.forEach(element => {
   element.addEventListener('click', () =>{ 
    // console.log("click");
     container.innerHTML="";
@@ -16,23 +16,31 @@ orderDos.forEach(element => {
   })
 })
 
+//FUNCION PARA ACHICAR IMAGEN -- INTENTO 
+function imageSize() {
+let image = document.getElementsByClassName("card-img-top");
+image.width="50";
+};
+
 // FUNCION MOSTRAR CARTAS 
 
  function addElement (info) { 
       info.forEach(element => {
         container.innerHTML += 
           `  
-           <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-2" >
-          <div class="card  border-dark text-center rounded-lg mb-3"  >
-          <p class="card-text">${element.num}</p>
-          <img src="${element.img}" class="card-img-top" alt="...">
+    <div class=" col-12  col-sm-6 col-md-4 col-lg-3 col-xl-2 " >
+       <div class="card  border-dark text-center rounded-lg mb-3"  >
+            <p class="card-text">${element.num}</p>
+              <img src="${element.img}" class="card-img-top" alt="...">
         
-          <h5 class="card-title">${element.name}</h5>
-            
-          <button type="button" id="info" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          Info
-        </button>
-        </div>
+              <h5 class="card-title">${element.name}</h5>
+          <div>  
+             <button type="button"  class="btn btn-primary " data-toggle="modal" data-target="#exampleModal">
+               Info
+             </button>
+          </div>
+          <br>
+       </div>
         
     </div>`
        
@@ -47,7 +55,7 @@ orderDos.forEach(element => {
     addElement(pokeData);
   });
   
-    
+
 
  
  // FUNCION MODAL  
